b9c40f5c8d0ec6493a8fc4a4a83ce4e6ebf16350
Laurens Hertzer <laurens.hertzer@stud.kbw.ch>
2026-05-31 21:56:26 +0200

    feat: quicksave commit

--- src/backend.rs
+++ src/backend.rs
@@ -1,10 +1,13 @@
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
+use sysinfo::{System, CpuRefreshKind, RefreshKind};
+
 
 pub enum Event {
     Input(crossterm::event::KeyEvent),
     Progress(f64),
+    CpuProgress(f64),
 }
 
 pub fn handle_input_events(tx: mpsc::Sender<Event>) {
@@ -26,3 +29,20 @@ pub fn run_background_thread(tx: mpsc::Sender<Event>) {
         tx.send(Event::Progress(progress)).unwrap();
     }
 }
+
+pub fn cpu_background_thread(tx: mpsc::Sender<Event>) { ;
+    let mut sys = System::new_all();
+
+    loop {
+        sys.refresh_cpu_all();
+        let cpu_usage = sys.global_cpu_usage();
+        let cpu_ratio = (cpu_usage as f64) / 100.0;
+        if tx.send(Event::CpuProgress(cpu_ratio)).is_err() {
+            break;
+        }
+        thread::sleep(Duration::from_millis(1000));
+    }
+
+}
+
+
