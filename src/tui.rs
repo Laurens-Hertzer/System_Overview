d1a79ee32fde043e1b8ef5c9aeb14824cbd4cd5d
Laurens-Hertzer <laurens.hertzer@stud.kbw.ch>
2026-06-22 11:53:22 +0200

    feat: 3 tabs in terminal, refactor: graph rendering is now a function in App and is referenced in impl widget app. Widget will now only be used for tabs

--- src/tui.rs
+++ src/tui.rs
@@ -1,17 +1,12 @@
 use crate::backend::Event;
-use crossterm::event::{KeyCode, KeyEventKind};
+use crossterm::event::{self, KeyCode, KeyEventKind};
 use std::collections::VecDeque;
 use std::io;
+use std::ptr::null;
 use std::sync::mpsc;
 
-use ratatui::{
-    DefaultTerminal, Frame,
-    layout::{Constraint, Layout},
-    style::{Color, Style, Stylize},
-    symbols::{border, Marker},
-    text::Line,
-    widgets::{Axis, Block, Chart, Dataset, GraphType, Widget},
-};
+use ratatui::{DefaultTerminal, Frame, layout::{Alignment, Constraint, Layout, Offset, Rect}, style::{Color, Style, Stylize}, symbols::{border, Marker}, text::{Line, Span}, widgets::{Axis, Block, Chart, Dataset, GraphType, Widget, Paragraph, Tabs}, symbols};
+use ratatui::prelude::Buffer;
 
 pub struct App {
     exit: bool,
@@ -80,7 +75,6 @@ impl App {
                 self.tab_selection = (self.tab_selection + 1) % 3;
             }
 
-            // Tab nach links (h oder Pfeiltaste Links)
             KeyCode::Char('h') | KeyCode::Left => {
                 self.tab_selection = (self.tab_selection + 2) % 3;
             }
@@ -89,30 +83,17 @@ impl App {
 
         Ok(())
     }
-}
 
-impl Widget for &App {
-    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
-    where
-        Self: Sized,
-    {
-        let vertical = Layout::vertical([
-            Constraint::Length(1),
-            Constraint::Percentage(50),
-            Constraint::Percentage(50),
-        ]);
-        let [title_area, top_area, bottom_area] = vertical.areas(area);
-
-        let horizontal = Layout::horizontal([
-            Constraint::Ratio(1, 3),
-            Constraint::Ratio(1, 3),
-            Constraint::Ratio(1, 3),
-        ]);
-        let [graph1_area, graph2_area, graph3_area] = horizontal.areas(top_area);
-        let [graph4_area, graph5_area, graph6_area] = horizontal.areas(bottom_area);
-
-        Line::from("Process overview").bold().render(title_area, buf);
-
+    fn render_resources(
+        &self,
+        graph1_area: Rect,
+        graph2_area: Rect,
+        graph3_area: Rect,
+        graph4_area: Rect,
+        graph5_area: Rect,
+        graph6_area: Rect,
+        buf: &mut Buffer,
+    ) {
         let current = self.cpu_history.back().copied().unwrap_or(0);
 
         let len = self.cpu_history.len() as f64;
@@ -150,5 +131,78 @@ impl Widget for &App {
             .x_axis(Axis::default().bounds([0.0, 60.0]).labels(x_labels))
             .y_axis(Axis::default().bounds([0.0, 100.0]).labels(y_labels))
             .render(graph1_area, buf);
+    }
+
+    fn render_containers(
+        &self,
+        area: Rect,
+        buf: &mut Buffer,
+    ) {
+        Paragraph::new("Containers")
+            .block(Block::bordered())
+            .render(area, buf);
+    }
+
+    fn render_network(
+        &self,
+        area: Rect,
+        buf: &mut Buffer,
+    ) {
+        Paragraph::new("Network")
+            .block(Block::bordered())
+            .render(area, buf);
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+    where
+        Self: Sized,
+    {
+        let vertical = Layout::vertical([
+            Constraint::Length(1),
+            Constraint::Percentage(100)
+        ]);
+        let [tabs_area, content_area] = vertical.areas(area);
+
+        let content_layout = Layout::vertical([
+            Constraint::Percentage(50),
+            Constraint::Percentage(50),
+        ]);
+
+        let [top_area, bottom_area] = content_layout.areas(content_area);
+
+        let horizontal_content_layout = Layout::horizontal([
+            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 3),
+        ]);
+        let [graph1_area, graph2_area, graph3_area] = horizontal_content_layout.areas(top_area);
+        let [graph4_area, graph5_area, graph6_area] = horizontal_content_layout.areas(bottom_area);
+
+        //let tab_selection = self.tab_selection;
+
+        Tabs::new(vec!["Resources", "Containers", "Network"])
+            .style(Color::White)
+            .highlight_style(Style::default().magenta().on_black().bold())
+            .select(self.tab_selection as usize)
+            .divider(symbols::DOT)
+            .padding(" ", " ")
+            .render(tabs_area, buf);
+
+        match self.tab_selection {
+            0 => self.render_resources(
+                graph1_area,
+                graph2_area,
+                graph3_area,
+                graph4_area,
+                graph5_area,
+                graph6_area,
+                buf,
+            ),
+            1 => self.render_containers(content_area, buf),
+            2 => self.render_network(content_area, buf),
+            _ => {}
+        }
     }
 }
